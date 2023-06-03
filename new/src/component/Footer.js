271cbdcc0b0f63de4b2b1f623cb568c0fcf983ec
VISALUJ <visaluj2000@gmail.com>
2023-06-03 08:15:49 +0530

    First commit

---
+++ new/src/component/Footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='conatiner text-center top'>
+          <div className='img'>
+            
+          </div>
+      
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
